81ed61441ed6388b660f736e82bd866e61454975
Melvin0070 <melvin.aspirant@gmail.com>
2023-08-30 23:00:46 +0530

    Done

--- game.js
+++ game.js
@@ -1,19 +1,89 @@
-// Iteration 1: Declare variables required for this game
+// eration 1: Declare variables required for this game
+const game_body = document.getElementById("game-body")
+let shotgun_sound = new Audio("./assets/shotgun.wav")
+let background_music = new Audio("./assets/bgm.mp3")
+let zombieID=0;
+let lives = 5
+let Zombie;
+let time = 60
 
-// Iteration 1.2: Add shotgun sound
 
-// Iteration 1.3: Add background sound
+function gameover(){
+    location.href = "game-over.html"
+}
 
-// Iteration 1.4: Add lives
+game_body.onclick = ()=>{
+    shotgun_sound.currentTime = 0
+    shotgun_sound.play()
+}
 
-// Iteration 2: Write a function to make a zombie
+background_music.play()
+background_music.loop = true
 
-// Iteration 3: Write a function to check if the player missed a zombie
 
-// Iteration 4: Write a function to destroy a zombie when it is shot or missed
+function genZ(){
+    game_body.innerHTML += `<img src=./assets/zombie-${generateUniqueNumber(1, 6)}.png class = zombie-image id = Zombies${zombieID}>`
+    Zombie = document.getElementById(`Zombies${zombieID}`)
 
-// Iteration 5: Creating timer
+    let second = generateUniqueNumber(1,6)
+    Zombie.style.animationDuration =`${second}s`
+    let viewWidth = generateUniqueNumber(20, 80)
+    Zombie.style.transform = `translateX(${viewWidth}vw)`
+    Zombie.onclick = ()=>{
+        destroyZombie(Zombie)
+
+    }
+    zombieID +=1
+}
+genZ()
+
+
 
 // Iteration 6: Write a code to start the game by calling the first zombie
 
+
 // Iteration 7: Write the helper function to get random integer
+function generateUniqueNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  }
+function destroyZombie(Zombie){
+    Zombie.style.display = "none"
+    zombieID +=1
+    genZ()
+}
+
+
+function zombieEscape(Zombie){
+
+    if(Zombie.getBoundingClientRect().top<=0){
+      const maxLivesElement = document.getElementById("max-lives");
+      maxLivesElement.style.width = lives*12+"%";
+        destroyZombie(Zombie)
+        lives -= 1
+        if(lives==0){
+            gameover()
+        }
+    }
+}
+
+zombieEscape(Zombie)
+
+setInterval(timer,1000)
+function timer(){
+    if(time<=0){
+        window.location.href = "win.html"
+
+    }else{
+        time--
+        clearInterval(timer)
+        document.getElementById("timer").innerText = time
+        zombieEscape(Zombie)
+    }
+
+}
+setInterval(check,100)
+function check() {
+    zombieEscape(Zombie);
+}
